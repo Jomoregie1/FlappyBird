999d993eedf079cf4fdd5c3925f1fb11d9e063de
Joseph <josephomoregie1@outlook.com>
2024-03-30 18:31:54 +0000

    added game logic

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,11 +1,12 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Objects;
-import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel{
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
 //    This adds in our images stored in the source files .
     Image backgroundImg;
@@ -14,32 +15,81 @@ public class FlappyBird extends JPanel{
     Image bottomPipeImg;
     Bird bird;
 
+//    Game Logic
+    Timer gameLoop;
+    int velocityY = -9;
+    int gravity = 1;
+
+
 
     FlappyBird() {
 //        sets the preferredSizes dimensions.
         setPreferredSize(new Dimension(Screen.getBOARD_WIDTH(),Screen.getBOARD_HEIGHT()));
-//        sets the background color to blue due to the background used.
-        setBackground(Color.blue);
 
 //        This initialises the fields, retrieving the images stored in the src file.
-        backgroundImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./flappybirdbg.png"))).getImage();
-        birdImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./flappybird.png"))).getImage();
-        topPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./toppipe.png"))).getImage();
-        bottomPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./bottompipe.png"))).getImage();
+        backgroundImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/flappybirdbg.png"))).getImage();
+        birdImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/flappybird.png"))).getImage();
+        topPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/toppipe.png"))).getImage();
+        bottomPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/bottompipe.png"))).getImage();
         this.bird = new Bird(birdImg);
+
+//      this creates a timer for our game loop, looping 60 times per second.
+        this.gameLoop = new Timer(1000/60, this);
+
+//      Starts the gameloop timer.
+        this.gameLoop.start();
+
     }
 
     public void paintComponent(Graphics g) {
+//        Used to call the superclasses method paintComponent, which is the component to paint.
         super.paintComponent(g);
         draw(g);
     }
 
     public void draw(Graphics g) {
+//       Draws the given image inside the space provided.
         g.drawImage(backgroundImg, 0,0, Screen.getBOARD_WIDTH(), Screen.getBOARD_HEIGHT(), null);
         g.drawImage(this.bird.getImg(), this.bird.getBirdX(), this.bird.getBirdY(), Bird.getBIRD_WIDTH(), Bird.getBIRD_HEIGHT(), null);
     }
 
 
+    public void move() {
+//        updating birds x and y's position
+        velocityY += gravity;
+        this.bird.increaseBirdY(this.velocityY);
+        this.bird.setBirdY(Math.max(this.bird.getBirdY(),0));
+    }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//        repaints the given component.
+        move();
+        repaint();
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
